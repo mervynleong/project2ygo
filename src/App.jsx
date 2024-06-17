03353680b0dadccc628122a8767afe71402b8ed6
mervynleong <changpeng66@gmail.com>
2024-06-17 21:05:33 +0800

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Display from "./components/BlueEyesDisplay";
 import { Form } from "react-router-dom";
 import BlueEyesDisplay from "./components/BlueEyesDisplay";
 import DarkMagicianDisplay from "./components/DarkMagicianDisplay";
+import Album from "./components/Album";
+import { useParams } from "react-router-dom";
+import { Suspense } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import NotFound from "./pages/NotFound";
 
 function App() {
   const [display, setDisplay] = useState([]);
@@ -31,6 +37,15 @@ function App() {
     <>
       {/* <DarkMagicianDisplay></DarkMagicianDisplay> */}
       <BlueEyesDisplay></BlueEyesDisplay>
+      <Album>{display}</Album>
+
+      {/* <Routes>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Route path="/" element={<Navigate replace to="/main" />} />
+          <Route path="main" element={<BlueEyesDisplay />} />
+          <Route path="*" element={<NotFound />} />
+        </Suspense>
+      </Routes> */}
     </>
   );
 }
